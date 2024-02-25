ecf9431a1aeadc7db8d0b84779562b1914bda0f9
Xena99 <138739917+Xena99@users.noreply.github.com>
2024-02-25 12:28:41 -0800

    Add files via upload
    adding

--- CS580HWView.cpp
+++ CS580HWView.cpp
@@ -6,10 +6,12 @@
 
 #include "CS580HWDoc.h"
 #include "CS580HWView.h"
+#include "RotateDlg.h"
+#include "TranslateDlg.h"
+#include "ScaleDlg.h"
 
-#include "Gz.h"
 #include "disp.h"
-#include "Application2.h"
+#include "Application4.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -25,6 +27,9 @@ IMPLEMENT_DYNCREATE(CCS580HWView, CView)
 BEGIN_MESSAGE_MAP(CCS580HWView, CView)
 	//{{AFX_MSG_MAP(CCS580HWView)
 	ON_COMMAND(IDM_RENDER, OnRender)
+	ON_COMMAND(IDM_ROTATE, OnRotate)
+	ON_COMMAND(IDM_TRANSLATE, OnTranslate)
+	ON_COMMAND(IDM_SCALE, OnScale)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -35,11 +40,14 @@ CCS580HWView::CCS580HWView()
 {
 	// TODO: add construction code here
 	m_pApplication = NULL;
-
 }
 
 CCS580HWView::~CCS580HWView()
 {
+	if(m_pApplication != NULL)
+	{
+		delete m_pApplication;
+	}
 }
 
 BOOL CCS580HWView::PreCreateWindow(CREATESTRUCT& cs)
@@ -90,11 +98,13 @@ void CCS580HWView::OnRender()
 {
 	// TODO: Add your command handler code here
 
-	// Call renderer using render context
+	// Call renderer 
 
-	// Application 2
-	m_pApplication = new Application2;
-	m_pApplication->Render();
+	// Application 4
+	if(m_pApplication != NULL)
+		((Application4 *)m_pApplication)->Render();
+	else 
+		AfxMessageBox("Application was not allocated\n");
 
 	// Set window size
 	CRect clientRect, windowRect;
@@ -117,6 +127,12 @@ void CCS580HWView::DrawFrameBuffer(CDC *pDC)
     {
         return;
     }
+
+	if(!m_pApplication->m_pRender->open)
+	{
+		AfxMessageBox("Renderer was not opened\n");
+		return;
+	}
 
     HDC hdc;
     hdc = ::CreateCompatibleDC(pDC->m_hDC);
@@ -154,5 +170,146 @@ void CCS580HWView::DrawFrameBuffer(CDC *pDC)
     ::BitBlt(pDC->m_hDC, 0, 0, m_pApplication->m_nWidth, m_pApplication->m_nHeight, 
         hdc, 0, 0, SRCCOPY);
     ::DeleteDC(hdc);
-    DeleteObject(m_bitmap);
+    DeleteObject(m_bitmap); 
+}
+
+void CCS580HWView::OnInitialUpdate() 
+{
+	CView::OnInitialUpdate();
+	
+	// TODO: Add your specialized code here and/or call the base class
+
+	// Assign Application 4
+	if(m_pApplication == NULL)
+	{
+		m_pApplication = new Application4;
+	}
+	
+	// Initialize and begin renderer
+	((Application4 *)m_pApplication)->Initialize();
+}
+
+// Callback function for rotation  
+void CCS580HWView::OnRotate() 
+{
+	// TODO: Add your command handler code here
+	CRotateDlg dlg;
+	GzInput* input;
+	GzMatrix	rotMat = 
+	{ 
+		1.0,	0.0,	0.0,	0.0, 
+		0.0,	1.0,	0.0,	0.0, 
+		0.0,	0.0,	1.0,	0.0, 
+		0.0,	0.0,	0.0,	1.0 
+	};
+
+
+	if(m_pApplication == NULL) return;
+
+	input = m_pApplication->m_pUserInput;
+	if(input == NULL) return;
+
+	// Initialize
+	input->rotation[0] = input->rotation[1] = input->rotation[2] = 0;
+	dlg.Initialize(input->rotation[0], input->rotation[1], input->rotation[2]);
+
+	if(dlg.DoModal() == IDOK)
+	{
+		// Update input rotation value
+		input->rotation[dlg.m_nAxis] = dlg.m_fRot;
+				
+		//  Create Rotation Matrix 
+		switch(dlg.m_nAxis)
+		{
+		case 0 :
+			// Create matrix for Rot X
+			GzRotXMat(input->rotation[0], rotMat);
+			break;
+		case 1:
+			// Create matrix for Rot Y
+			GzRotYMat(input->rotation[1], rotMat);
+			break;
+		case 2:
+			// Create matrix for Rot Z
+			GzRotZMat(input->rotation[2], rotMat);
+			break;
+		}
+
+		// Accumulate matrix
+		GzPushMatrix(m_pApplication->m_pRender, rotMat, true); 
+	}
+}
+
+// Callback function for Translation
+void CCS580HWView::OnTranslate() 
+{
+	// TODO: Add your command handler code here
+	CTranslateDlg dlg;
+	GzInput* input;
+	GzMatrix	trxMat = 
+	{ 
+		1.0,	0.0,	0.0,	0.0, 
+		0.0,	1.0,	0.0,	0.0, 
+		0.0,	0.0,	1.0,	0.0, 
+		0.0,	0.0,	0.0,	1.0 
+	};
+
+
+	if(m_pApplication == NULL) return;
+
+	input = m_pApplication->m_pUserInput;
+	if(input == NULL) return;
+
+	// Initialize
+	input->translation[0] = input->translation[1] = input->translation[2] = 0;
+	dlg.Initialize(input->translation[0], input->translation[1], input->translation[2]);
+
+	if(dlg.DoModal() == IDOK)
+	{
+		// Update input translation value
+		input->translation[0] = dlg.m_fTx; input->translation[1] = dlg.m_fTy; input->translation[2] = dlg.m_fTz;
+
+		//  Create Translation Matrix
+		GzTrxMat(input->translation, trxMat);
+
+		// Accumulate matrix
+		GzPushMatrix(m_pApplication->m_pRender, trxMat, false); 
+	}
+}
+
+// Callback function for Scaling
+void CCS580HWView::OnScale() 
+{
+	// TODO: Add your command handler code here
+	CScaleDlg dlg;
+	GzInput* input;
+	GzMatrix scaleMat = 
+	{ 
+		1.0,	0.0,	0.0,	0.0, 
+		0.0,	1.0,	0.0,	0.0, 
+		0.0,	0.0,	1.0,	0.0, 
+		0.0,	0.0,	0.0,	1.0 
+	};
+
+
+	if(m_pApplication == NULL) return;
+
+	input = m_pApplication->m_pUserInput;
+	if(input == NULL) return;
+
+	// Initialize
+	input->scale[0] = input->scale[1] = input->scale[2] = 1;
+	dlg.Initialize(input->scale[0], input->scale[1], input->scale[2]);
+
+	if(dlg.DoModal() == IDOK)
+	{
+		// Update input scale value
+		input->scale[0] = dlg.m_fSx; input->scale[1] = dlg.m_fSy; input->scale[2] = dlg.m_fSz;
+
+		//  Create Scaling Matrix
+		GzScaleMat(input->scale, scaleMat);
+
+		// Accumulate matrix
+		GzPushMatrix(m_pApplication->m_pRender, scaleMat, true); 
+	}
 }
